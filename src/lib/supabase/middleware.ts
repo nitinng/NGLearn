fc556bae34ae72a226a5989e73817b4a4c2d1224
Nitin Sudarshan <nitinsudarshan@gmail.com>
2026-07-18 02:01:19 +0530

    feat: uncomment middleware redirect and make /p/ routes public

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -38,12 +38,13 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/p/')
   ) {
     // no user, potentially respond by redirecting the user to the login page
-    // const url = request.nextUrl.clone()
-    // url.pathname = '/login'
-    // return NextResponse.redirect(url)
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
